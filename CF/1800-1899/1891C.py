32a986703967c2c090b89b4de6e1ed711915aa12
FancyALad <95732059+FancyALad@users.noreply.github.com>
2024-07-16 10:04:55 +0800

    Update 1891C.py

--- CF/1800-1899/1891C.py
+++ CF/1800-1899/1891C.py
@@ -3,7 +3,10 @@ def solve():
     nums = list(map(int, input().split()))
     s = s_2 = (sum(nums) + 1) // 2
     nums.sort()
+    
     i = 0
+    # 一个数的一半不会大于等于这个数自身，但是一个数 +1 再除 2 就有可能了。
+    # 特例是 1，（1 + 1）// 2 = 1。所以当 n = 1，nums = [1] 时如果不加 i < n 会越界。
     while i < n and s_2 - nums[i] >= 0:
         s_2 -= nums[i]
         i += 1
