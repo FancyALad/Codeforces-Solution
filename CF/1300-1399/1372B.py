35e7a9b597df9fd57a7802d34fb33864b9920594
FancyALad <FancyALad@github.com>
2024-07-22 12:42:43 +0800

    create 1372B.py

---
+++ CF/1300-1399/1372B.py
@@ -0,0 +1,30 @@
+from math import sqrt
+import sys
+input = lambda: sys.stdin.readline().strip()
+check = int(input())
+'''
+def LCD(a: int, b: int) -> int:
+    c = b % a
+    if c == 0:
+        return a
+    if c > a:
+        a, c = c, a
+    return LCD(c, a)
+
+def LCM(a: int, b: int) -> int:
+    return a * b // LCD(a, b)
+'''
+
+for _ in range(check):
+    n = int(input())
+    a = 1; b = n - 1
+    k = 0
+    for i in range(2, int(sqrt(n)) + 1):
+        if n % i == 0:
+            k = n // i 
+            break
+    if k:
+        a = k
+        b = n - k
+            
+    print(a, b)
